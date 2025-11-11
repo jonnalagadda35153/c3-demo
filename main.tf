d3581ad213c6c5574a6f0ddbb79ebadbdd4dbacd
jonnalagadda53153 <jaswanthkumar54321@gmail.com>
2025-11-11 15:22:46 -0500

    Bad syntax and config tf files

--- main.tf
+++ main.tf
@@ -42,8 +42,10 @@ resource "aws_s3_bucket" "demo" {
 resource "aws_s3_bucket_public_access_block" "demo" {
   bucket = aws_s3_bucket.demo.id
 
-  block_public_acls       = true
+  block_public_acls       = false
   ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
+  block_public_policy     = false
+  restrict_public_buckets = false
 }
+
+bad syntax
