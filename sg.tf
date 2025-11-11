d72b6b387b0f80f7257255bdbbc3f5ab95959289
jonnalagadda53153 <jaswanthkumar54321@gmail.com>
2025-11-11 14:00:32 -0500

    Add sg bad file

---
+++ sg.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAFAKE"
+  secret_key = "FAKESECRET"
+}
+
+resource "aws_security_group" "wide_open" {
+  name        = "demo-wide-open"
+  description = "Wide open SG"
+  vpc_id      = "vpc-123456"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "demo-wide-open"
+  }
+}
