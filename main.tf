319a883a4f72d665fee55f244b4a69b4b83a5a6b
jonnalagadda53153 <jaswanthkumar54321@gmail.com>
2025-11-18 14:25:58 +0000

    automated fixes for main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-11231"
+  region = "us-east-1"
   # Use AWS profile or assume-role (no credentials in code)
 }
 
@@ -42,8 +42,8 @@ resource "aws_s3_bucket" "demo" {
 resource "aws_s3_bucket_public_access_block" "demo" {
   bucket = aws_s3_bucket.demo.id
 
-  block_public_acls       = false
+  block_public_acls       = true
   ignore_public_acls      = true
-  block_public_policy     = false
+  block_public_policy     = true
   restrict_public_buckets = true
 }
